417d59baa77cf4a4389a1ad66b6a3d6dae07613c
James Hamilton McRoberts IV <jamesmcroberts82997@gmail.com>
2020-02-01 12:43:26 -0800

    Added a firing state options

--- robotContol.h
+++ robotContol.h
@@ -1,9 +1,19 @@
 #ifndef GENERAL_ROBOT_CONTROL_H
 #define GENERAL_ROBOT_CONTROL_H
 
-/*
-All values are in the range [-1.0, 1.0]
-*/
+typedef uint8_t FIRING_FLAGS;
+
+enum firingFlags {
+    FIRING_none = 0,
+
+    FIRING_primary = 1u << 0u,
+    FIRING_secondary = 1u << 2u
+};
+
+/**
+ * Holds all different control states for a robot.
+ * All states are in the range [-1.0, 1.0].
+ */
 typedef struct {
     float base_forward;             // +: Forward; -: Backward
     float base_strafe;              // +: Right; -: Left
@@ -14,6 +24,8 @@ typedef struct {
 
     float secondaryGun_pitch;       // +: Up; -: Down
     float secondaryGun_rotation;    // +: Clockwise; -: Counter-Clockwise
+
+    FIRING_FLAGS firing;
 } ROBOT_CONTROL_BULK;
 
 #endif
